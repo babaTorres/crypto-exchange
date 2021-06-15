21ac893a356278da1cd659a58cb122da00a9b43f
Brandon Torres <torresbm95@gmail.com>
2021-06-15 16:02:37 -0400

    Completed Trade Component

--- src/store/actions.js
+++ src/store/actions.js
@@ -27,4 +27,26 @@ export function web3Loaded(connection) {
       contract
     }
   }
+
+export function cancelledOrdersLoaded(cancelledOrders){
+  return {
+    type: 'CANCELLED_ORDERS_LOADED',
+    cancelledOrders
+  }
+}
+
+export function filledOrdersLoaded(filledOrders){
+  return {
+    type: 'FILLED_ORDERS_LOADED',
+    filledOrders
+  }
+}
+
+export function allOrdersLoaded(allOrders){
+  return {
+    type: 'ALL_ORDERS_LOADED',
+    allOrders
+  }
+}
   
+
